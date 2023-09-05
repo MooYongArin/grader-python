e3444c8fabcf7195c6d6ac4d0f90ebe1916759cd
MooYongArin <yongariyata@gmail.com>
2023-09-05 16:16:34 +0700

    HW_list

--- 05-List/31_List_★★_Cut_n_Shuffle.py
+++ 05-List/31_List_★★_Cut_n_Shuffle.py
@@ -1,11 +1,31 @@
 count_card = 0
 card = input().split()
+saved_cards = []
 cutNshuffle = input()
 
 for x in card:
+    saved_cards.append(x)
     count_card = count_card + 1
 
-for i in range(1,count_card):
-    card[i-1] = card[count_card-i]
+for method in cutNshuffle:
+    if method == "C":
+        # Cut
+        for i in range(0,count_card):
+            if i < int(0.5*count_card):
+                card[i] = saved_cards[i+int(0.5*count_card)]
+            elif i >= int(0.5*count_card):
+                card[i] = saved_cards[i-int(0.5*count_card)]
+        saved_cards = []
+        for x in card:
+            saved_cards.append(x)
+    elif method == "S":
+        # Shuffle
+        for i in range(0,count_card):
+            if i < int(0.5*count_card):
+                card[2*i] = saved_cards[i]
+                card[(2*i)+1] = saved_cards[i+int(0.5*count_card)]
+        saved_cards = []
+        for x in card:
+            saved_cards.append(x)
 
-print(card)
+print(' '.join(card))
---
+++ 05-List/F22_List_★★_Upgrade_2 (Function).py
@@ -0,0 +1,9 @@
+def index_of(grades, ID):
+    
+    return 0
+def upgrade(grades, IDs):
+    return 0
+
+exec(input())
+exec(input())
+exec(input())
